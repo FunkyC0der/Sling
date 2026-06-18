654e2603fce7f0e2277e4181d0382b8f7bbc529c
Yehor Krasochenko <krasochenko.dev@gmail.com>
2026-06-18 18:34:28 +0300

    Add wind vfx  * set proper physics layer  * add gravity scale effect to gravity zone

--- Assets/_Game/Scripts/Level/Elements/GravityZone/GravityZone.cs
+++ Assets/_Game/Scripts/Level/Elements/GravityZone/GravityZone.cs
@@ -7,18 +7,24 @@ namespace Sling.Level.Elements.GravityZone
   {
     public GravityZoneConfig Config;
     
-    private readonly List<Rigidbody2D> _rigidbodies = new();
+    private readonly HashSet<Rigidbody2D> _rigidbodies = new();
 
     private void OnTriggerEnter2D(Collider2D other)
     {
       if(other.attachedRigidbody)
-        _rigidbodies.Add(other.attachedRigidbody);
+      {
+        if(_rigidbodies.Add(other.attachedRigidbody))
+          other.attachedRigidbody.gravityScale *= Config.GravityScaleMultiplier;
+      }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
       if(other.attachedRigidbody)
-        _rigidbodies.Remove(other.attachedRigidbody);
+      {
+        if (_rigidbodies.Remove(other.attachedRigidbody)) 
+          other.attachedRigidbody.gravityScale /= Config.GravityScaleMultiplier;
+      }
     }
 
     private void FixedUpdate()
--- Assets/_Game/Scripts/Level/Elements/GravityZone/GravityZoneConfig.cs
+++ Assets/_Game/Scripts/Level/Elements/GravityZone/GravityZoneConfig.cs
@@ -6,5 +6,8 @@ namespace Sling.Level.Elements.GravityZone
   public class GravityZoneConfig : ScriptableObject
   {
     public float Force = 2f;
+
+    [Range(0, 1)]
+    public float GravityScaleMultiplier = 0.5f;
   }
 }
