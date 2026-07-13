8d6cecedb745a9a65676e785ce85fd2c166d86a3
Yehor Krasochenko <krasochenko.dev@gmail.com>
2026-07-13 16:35:07 +0300

    Recenter TestMap in Level_Test scene

---
+++ Assets/_Game/Scripts/Editor/TiledMapCenteringImporter.cs
@@ -0,0 +1,27 @@
+using SuperTiled2Unity;
+using SuperTiled2Unity.Editor;
+using UnityEngine;
+
+namespace Sling.Editor
+{
+  [AutoCustomTmxImporter(100)]
+  public sealed class TiledMapCenteringImporter : CustomTmxImporter
+  {
+    public override void TmxAssetImported(TmxAssetImportedArgs args)
+    {
+      SuperMap map = args.ImportedSuperMap;
+      if (map.m_Orientation != MapOrientation.Orthogonal || map.m_Infinite)
+        return;
+
+      Grid grid = map.GetComponentInChildren<Grid>(true);
+      if (grid == null)
+        throw new CustomImporterException($"Map '{map.name}': imported Grid was not found.");
+
+      float inversePixelsPerUnit = args.AssetImporter.InversePPU;
+      float width = map.m_Width * map.m_TileWidth * inversePixelsPerUnit;
+      float height = map.m_Height * map.m_TileHeight * inversePixelsPerUnit;
+
+      grid.transform.localPosition = new Vector3(-width * 0.5f, height * 0.5f, 0f);
+    }
+  }
+}
