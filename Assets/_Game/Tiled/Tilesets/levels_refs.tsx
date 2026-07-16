978bece27805399c3e0f9661024690cd02e8e9a1
Yehor Krasochenko <krasochenko.dev@gmail.com>
2026-07-16 17:05:12 +0300

    Switch to new tiles for Level 1-5

---
+++ Assets/_Game/Tiled/Tilesets/levels_refs.tsx
@@ -0,0 +1,94 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="levels_refs" tilewidth="320" tileheight="180" tilecount="30" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../Generated/LevelPreviews/Level_2.png" width="320" height="180"/>
+ </tile>
+ <tile id="1">
+  <image source="../../Generated/LevelPreviews/Level_3.png" width="320" height="180"/>
+ </tile>
+ <tile id="2">
+  <image source="../../Generated/LevelPreviews/Level_10.png" width="320" height="180"/>
+ </tile>
+ <tile id="3">
+  <image source="../../Generated/LevelPreviews/Level_1.png" width="320" height="180"/>
+ </tile>
+ <tile id="4">
+  <image source="../../Generated/LevelPreviews/Level_Boss2.png" width="320" height="180"/>
+ </tile>
+ <tile id="5">
+  <image source="../../Generated/LevelPreviews/Level_11.png" width="320" height="180"/>
+ </tile>
+ <tile id="6">
+  <image source="../../Generated/LevelPreviews/Level_Boss1.png" width="320" height="180"/>
+ </tile>
+ <tile id="7">
+  <image source="../../Generated/LevelPreviews/Level_9.png" width="320" height="180"/>
+ </tile>
+ <tile id="8">
+  <image source="../../Generated/LevelPreviews/Level_8.png" width="320" height="180"/>
+ </tile>
+ <tile id="9">
+  <image source="../../Generated/LevelPreviews/Level_7.png" width="320" height="180"/>
+ </tile>
+ <tile id="10">
+  <image source="../../Generated/LevelPreviews/Level_6.png" width="320" height="180"/>
+ </tile>
+ <tile id="11">
+  <image source="../../Generated/LevelPreviews/Level_5.png" width="320" height="180"/>
+ </tile>
+ <tile id="12">
+  <image source="../../Generated/LevelPreviews/Level_4.png" width="320" height="180"/>
+ </tile>
+ <tile id="13">
+  <image source="../../Generated/LevelPreviews/Level_28.png" width="320" height="180"/>
+ </tile>
+ <tile id="14">
+  <image source="../../Generated/LevelPreviews/Level_27.png" width="320" height="180"/>
+ </tile>
+ <tile id="15">
+  <image source="../../Generated/LevelPreviews/Level_26.png" width="320" height="180"/>
+ </tile>
+ <tile id="16">
+  <image source="../../Generated/LevelPreviews/Level_25.png" width="320" height="180"/>
+ </tile>
+ <tile id="17">
+  <image source="../../Generated/LevelPreviews/Level_24.png" width="320" height="180"/>
+ </tile>
+ <tile id="18">
+  <image source="../../Generated/LevelPreviews/Level_23.png" width="320" height="180"/>
+ </tile>
+ <tile id="19">
+  <image source="../../Generated/LevelPreviews/Level_22.png" width="320" height="180"/>
+ </tile>
+ <tile id="20">
+  <image source="../../Generated/LevelPreviews/Level_21.png" width="320" height="180"/>
+ </tile>
+ <tile id="21">
+  <image source="../../Generated/LevelPreviews/Level_20.png" width="320" height="180"/>
+ </tile>
+ <tile id="22">
+  <image source="../../Generated/LevelPreviews/Level_19.png" width="320" height="180"/>
+ </tile>
+ <tile id="23">
+  <image source="../../Generated/LevelPreviews/Level_18.png" width="320" height="180"/>
+ </tile>
+ <tile id="24">
+  <image source="../../Generated/LevelPreviews/Level_17.png" width="320" height="180"/>
+ </tile>
+ <tile id="25">
+  <image source="../../Generated/LevelPreviews/Level_16.png" width="320" height="180"/>
+ </tile>
+ <tile id="26">
+  <image source="../../Generated/LevelPreviews/Level_15.png" width="320" height="180"/>
+ </tile>
+ <tile id="27">
+  <image source="../../Generated/LevelPreviews/Level_14.png" width="320" height="180"/>
+ </tile>
+ <tile id="28">
+  <image source="../../Generated/LevelPreviews/Level_13.png" width="320" height="180"/>
+ </tile>
+ <tile id="29">
+  <image source="../../Generated/LevelPreviews/Level_12.png" width="320" height="180"/>
+ </tile>
+</tileset>
