3ede731843a4c406c39980714adb9fd0438e944e
Yehor Krasochenko <krasochenko.dev@gmail.com>
2026-07-18 15:31:56 +0300

    Update b1 tileset  * Add new flag for finish zone

--- Assets/_Game/Tiled/Tilesets/b1.tsx
+++ Assets/_Game/Tiled/Tilesets/b1.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.2" name="b1" tilewidth="16" tileheight="16" tilecount="60" columns="10" tilerendersize="grid">
- <image source="../Sprites/b1.png" width="160" height="96"/>
+<tileset version="1.10" tiledversion="1.12.2" name="b1" tilewidth="16" tileheight="16" tilecount="63" columns="9" tilerendersize="grid">
+ <image source="../Sprites/b1.png" width="144" height="112"/>
 </tileset>
