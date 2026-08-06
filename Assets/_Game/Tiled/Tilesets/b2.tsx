4616d5feae82caf59f41c4645adcaea3ec2d4ee8
Yehor Krasochenko <krasochenko.dev@gmail.com>
2026-08-06 11:00:53 +0300

    Duplicate initial files for second biome

---
+++ Assets/_Game/Tiled/Tilesets/b2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.12.2" name="b2" tilewidth="16" tileheight="16" tilecount="63" columns="9">
+ <image source="../Sprites/b2.png" width="144" height="112"/>
+</tileset>
