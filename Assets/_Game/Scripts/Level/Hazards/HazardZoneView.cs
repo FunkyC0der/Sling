e0acdc8a15acb402c7dedda8dbd70928f4ac79b6
Yehor Krasochenko <krasochenko.dev@gmail.com>
2026-08-25 15:37:47 +0300

    Add hazard zone component to sand wall

--- Assets/_Game/Scripts/Level/Hazards/HazardZoneView.cs
+++ Assets/_Game/Scripts/Level/Hazards/HazardZoneView.cs
@@ -5,6 +5,9 @@ namespace Sling.Level.Hazards
   public class HazardZoneView : MonoBehaviour
   {
     private void OnTriggerEnter2D(Collider2D other) =>
-      other?.GetComponent<IDamageable>()?.TakeDamage();    
+      other?.attachedRigidbody?.GetComponent<IDamageable>()?.TakeDamage();
+
+    private void OnCollisionEnter2D(Collision2D other) => 
+      other?.collider.attachedRigidbody?.GetComponent<IDamageable>()?.TakeDamage();
   }
 }
